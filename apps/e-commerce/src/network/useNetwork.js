14c8bd3c00ec533f06f86d465c554c867f8da378
adilkadivala <adilkadivala560@gmail.com>
2024-09-09 06:49:37 +0530

    creating more reliable network panel

--- apps/e-commerce/src/network/useNetwork.js
+++ apps/e-commerce/src/network/useNetwork.js
@@ -1,6 +1,31 @@
 import { useState } from 'react';
 
+// use this function if you have a Local Json
+function useNetwork() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  function get(apiData) {
+    setIsLoading(true);
+    setError('');
+    try {
+      setData(apiData.data);
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return { get, data, isLoading, error };
+}
+export default useNetwork;
+
+
 // use this function if you haven't local JSON,
+
 // function useNetwork() {
 //   const [data, setData] = useState(null);
 //   const [isLoading, setIsLoading] = useState(false);
@@ -27,26 +52,51 @@ import { useState } from 'react';
 //   return { get, data, isLoading, error };
 // }
 
-// use this function if you have a Local Json
-function useNetwork() {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
 
-  function get(apiData) {
-    setIsLoading(true);
-    setError('');
-    try {
-      setData(apiData.data);
-    } catch (err) {
-      console.log(err);
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  }
 
-  return { get, data, isLoading, error };
-}
 
-export default useNetwork;
+// best way
+// import { axios } from 'axios';
+
+
+// class APISERVICE {
+//   static APIInstance; // if we have 10 apies, then it will prevent to create 10 instance,  it will create 1 for 10;
+
+//   constructor() {
+//     this.axiosInstance = axios.create({
+//       baseURL: 'http://localhost:4000/',
+//       timeOUT: 1000,
+//       headers: { 'custom-Header': 'token' },
+//     });
+//   }
+
+//   static getInstance() {
+//     if (!APISERVICE.APIInstance) {
+//       APISERVICE.APIInstance = new APISERVICE();
+//     }
+//   }
+
+//   get(endpoint) {
+//     // getting logic
+//   }
+//   post(endpoint) {
+//     // posting logic
+//   }
+//   put(endpoint) {
+//     // updating logic
+//   }
+//   delete(endpoint) {
+//     // deleting logic
+//   }
+
+//   cancleApiCall() {}
+// }
+
+//  if we want to over write some method for any perticular api ,
+
+// class productAPI extends APISERVICE {
+//   put() {}
+// }
+
+// const productapies = new productAPI();
+// productapies.get('endpoint');
